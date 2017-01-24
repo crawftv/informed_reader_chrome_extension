1321bf059b3008e36617f7a73a48880c20f12c4f
crawdogginit <crawford.collins@gmail.com>
2017-01-23 20:04:52 -0600

    put the information in brackets'

--- content_script.js
+++ content_script.js
@@ -34,11 +34,11 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bBruce Chassy\b/gi, "Bruce Chassy, a University of Illinois professor who has recieved funding from Monsanto, ");
-	v = v.replace(/\bKevin Folta\b/gi, "Kevin Folta, a University of Florida professor who has recieved funding from Monsanto, ");
-	v = v.replace(/\bKevin M. Folta\b/gi, "Kevin M. Folta, a University of Florida professor who has recieved funding from Monsanto, ");
-	v = v.replace(/\bTaking on the Food Industry, One Blog Post at a Time\b/gi, "Taking on the Food Industry, One Blog Post at a Time. This article references the works of professors who have been funded by Monsanto.");
-	v = v.replace(/\bFoes of Modified Corn Find Support in a Study\b/gi, "Foes of Modified Corn Find Support in a Study. This article references the works of professors who have been funded by Monsanto.");
+	v = v.replace(/\bBruce Chassy\b/gi, "[Bruce Chassy, a University of Illinois professor who has recieved funding from Monsanto]");
+	v = v.replace(/\bKevin Folta\b/gi, "[Kevin Folta, a University of Florida professor who has recieved funding from Monsanto]");
+	v = v.replace(/\bKevin M. Folta\b/gi, "[Kevin M. Folta, a University of Florida professor who has recieved funding from Monsanto]");
+	v = v.replace(/\bTaking on the Food Industry, One Blog Post at a Time\b/gi, "[Taking on the Food Industry, One Blog Post at a Time. This article references the works of professors who have been funded by Monsanto.]");
+	v = v.replace(/\bFoes of Modified Corn Find Support in a Study\b/gi, "[Foes of Modified Corn Find Support in a Study. This article references the works of professors who have been funded by Monsanto.]");
 
 	textNode.nodeValue = v;
 }
